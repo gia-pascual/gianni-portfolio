16586d43ba9664ae2a0649021585a93856f6a1d1
gia-pascual <giannilycapascual@gmail.com>
2026-08-27 05:25:51 +0800

    Delete gianni-portfolio/src/components directory

--- gianni-portfolio/src/components/LedgerRule.tsx
+++
@@ -1,9 +0,0 @@
-export default function LedgerRule({ label }: { label: string }) {
-  return (
-    <div className="ledger-rule mb-5">
-      <span className="font-mono text-xs uppercase tracking-[0.2em] text-gold-600 whitespace-nowrap">
-        {label}
-      </span>
-    </div>
-  );
-}
